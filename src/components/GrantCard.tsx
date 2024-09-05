13447edb38785529cc39111173ce4daf21bb8d33
George Kanellos <george.kanellos@boldplay.com>
2024-09-05 12:16:35 +0400

    small improvements for cards

--- src/components/GrantCard.tsx
+++ src/components/GrantCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heart, ThumbsUp, ThumbsDown, Github } from 'lucide-react';
+import { Heart, ThumbsUp, ThumbsDown, Github, Coins } from 'lucide-react';
 
 const GrantCard = ({ grant }) => {
 	return (
@@ -9,8 +9,8 @@ const GrantCard = ({ grant }) => {
 					<Github className="w-10 h-10 text-black mr-2" />
 				</div>
 				<div className="flex space-x-2">
-					<ThumbsUp className="w-5 h-5 text-gray-400" />
-					<ThumbsDown className="w-5 h-5 text-gray-400" />
+					<ThumbsUp className="w-5 h-5 text-gray-400 hover:text-orange-400" />
+					<ThumbsDown className="w-5 h-5 text-gray-400 hover:text-orange-400" />
 				</div>
 			</div>
 			<span className="text-gray-600 font-medium">
@@ -19,24 +19,8 @@ const GrantCard = ({ grant }) => {
 			<h2 className="text-xl text-gray-800 mb-4">{grant.title}</h2>
 
 			<div className="flex mb-4">
-				<div className="w-1/2 bg-orange-50 rounded-lg p-3 mr-2">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="24"
-						height="24"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						className="lucide lucide-coins w-6 h-6 text-orange-400 mb-1"
-					>
-						<circle cx="8" cy="8" r="6" />
-						<path d="M18.09 10.37A6 6 0 1 1 10.34 18" />
-						<path d="M7 6h1v4" />
-						<path d="m16.71 13.88.7.71-2.82 2.82" />
-					</svg>
+				<div className="w-1/2 bg-orange-50 rounded-lg p-3 mr-2 flex flex-wrap items-end">
+					<Coins className="lucide lucide-coins w-6 h-6 text-orange-400 mb-1" />
 					<div className="text-2xl font-bold text-orange-400">
 						{grant.avgAmount.toLocaleString()}
 					</div>
